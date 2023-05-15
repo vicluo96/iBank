6971344eb9ac98dae707f0b9eff7930fac824916
DinjerChang <tingtsechang@gmail.com>
2023-05-15 11:39:04 -0700

    update bank_dump.sql

--- db/bank_dump.sql
+++ db/bank_dump.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `user`
 --
-
+DROP SCHEMA IF EXISTS bank;
+CREATE SCHEMA bank;
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
